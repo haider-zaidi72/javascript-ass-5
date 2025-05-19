78c3a10a3ed07f0624a3c4f37e978d9d80907aab
urooj haider <suhz1100@gmail.com>
2025-05-19 16:56:49 +0500

    add

---
+++ java.js
@@ -0,0 +1,21 @@
+    
+    function colorChange (){
+    
+    let num = '123456789abcdef';
+    color = "#"
+  //  6 time loop chalaya ga takey 6 times random number generate ho
+    for(let i= 0; i< 6; i++){
+
+        //pehley ham ne random number generate kia hai phir num se multiply kia hai
+       let randomChar = num[Math.floor(Math.random()*num.length)]
+       color += randomChar  // output #123456
+     
+    //     // letter = Math.floor(Math.random()*6)+97
+
+        let text=document.getElementById("color") // get element by ID
+        text.style.backgroundColor= color;  // apply on background color
+    }
+     
+}
+
+
